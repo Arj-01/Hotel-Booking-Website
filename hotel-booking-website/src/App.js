24ee7fd02835a3f12488b7f7bfcf31c96640df89
Arj-01 <aarjitgiri@gmail.com>
2022-08-09 00:07:08 +0530

    name change

--- hotel-booking-website/src/App.js
+++ hotel-booking-website/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and React.
+          Edit <code>src/App.js</code> and latest Change.
         </p>
         <a
           className="App-link"
