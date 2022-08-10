2585b92b72ea94cf17bb84594847ac691000cfe3
Pradumn Patidar <pradumpatidar@gmail.com>
2022-08-11 00:00:14 +0530

    nameChange

--- hotel-booking-website/src/App.js
+++ hotel-booking-website/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Welcome to my React Project.
+          Edit <code>src/App.js</code> Welcome my React.
         </p>
         <a
           className="App-link"
