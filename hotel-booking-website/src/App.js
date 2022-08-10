def1d6b4d7df32af8ab5f94d369a1a65b2c5705c
Pradumn Patidar <pradumpatidar@gmail.com>
2022-08-10 23:56:38 +0530

    pCommit

--- hotel-booking-website/src/App.js
+++ hotel-booking-website/src/App.js
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and latest Change.
+          Edit <code>src/App.js</code> Welcome to my React Project.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        > 
           Learn React
         </a>
       </header>
