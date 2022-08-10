a631c6ad576f7b22de4564d529dac3d9682d1606
Arj-01 <aarjitgiri@gmail.com>
2022-08-10 23:51:04 +0530

    firstChange

--- hotel-booking-website/src/App.js
+++ hotel-booking-website/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and latest Change.
+          Edit <code>src/App.js</code> Welcome to React.
         </p>
         <a
           className="App-link"
